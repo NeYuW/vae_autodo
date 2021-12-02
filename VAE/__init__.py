6bc996c4b016ae0e27c2d049d0a396cf16fadbf9
NeYuW <73571491+NeYuW@users.noreply.github.com>
2021-12-02 10:30:31 -0600

    Add files via upload

---
+++ VAE/__init__.py
@@ -0,0 +1,26 @@
+from .operation import *
+from .base import *
+from .vanilla_vae import *
+# from .gamma_vae import *
+# from .beta_vae import *
+# from .wae_mmd import *
+# from .cvae import *
+# from .hvae import *
+# from .vampvae import *
+# from .iwae import *
+# from .dfcvae import *
+# from .mssim_vae import MSSIMVAE
+# from .fvae import *
+# from .cat_vae import *
+# from .joint_vae import *
+# from .info_vae import *
+# # from .twostage_vae import *
+# from .lvae import LVAE
+# from .logcosh_vae import *
+# from .swae import *
+# from .miwae import *
+# from .vq_vae import *
+# from .betatc_vae import *
+# from .dip_vae import *
+
+
