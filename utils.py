d1a447ba7de1093ce2f9cedb9b90ad731bfec4c4
NeYuW <73571491+NeYuW@users.noreply.github.com>
2021-12-20 16:53:23 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import torch
 
-__all__ = ['init_seeds', 'AverageMeter']
+__all__ = ['init_seeds', 'warmup_learning_rate', 'adjust_learning_rate']
 
 def init_seeds(seed=0):
     random.seed(seed)
@@ -12,20 +12,28 @@ def init_seeds(seed=0):
     torch.cuda.manual_seed_all(seed)
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
+def warmup_learning_rate(args, epoch, batch_id, total_batches, optimizer):
+    if args.lr_warm and epoch < args.lr_warm_epochs:
+        p = (batch_id + epoch * total_batches) / \
+            (args.lr_warm_epochs * total_batches)
+        lr = args.lr_warmup_from + p * (args.lr_warmup_to - args.lr_warmup_from)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+    #
+    for param_group in optimizer.param_groups:
+        lrate = param_group['lr']
+    return lrate
 
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
+def adjust_learning_rate(args, optimizer, epoch):
+    lr = args.lr
+    if args.lr_cosine:
+        eta_min = lr * (args.lr_decay_rate ** 3)
+        lr = eta_min + (lr - eta_min) * (
+                1 + math.cos(math.pi * epoch / args.epochs)) / 2
+    else:
+        steps = np.sum(epoch >= np.asarray(args.lr_decay_epochs))
+        if steps > 0:
+            lr = lr * (args.lr_decay_rate ** steps)
 
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+    for param_group in optimizer.param_groups:
 
